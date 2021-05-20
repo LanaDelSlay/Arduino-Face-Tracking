a146a8f09dffc6ea3af4dbd619f1d4df1b389d8b
LanaDelSlay <42992759+LanaDelSlay@users.noreply.github.com>
2021-05-20 02:49:41 -0700

    Arduino Face Tracking initial commit

---
+++ face.py
@@ -0,0 +1,51 @@
+#import all the required modules
+import numpy as np
+import serial
+import time
+import sys
+import cv2
+#Setup Communication path for arduino (In place of 'COM5' put the port to which your arduino is connected)
+arduino = serial.Serial('COM4', 115200) 
+time.sleep(2)
+print("Connected to arduino...")
+#importing the Haarcascade for face detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#To capture the video stream from webcam.
+cap = cv2.VideoCapture(0)
+#Read the captured image, convert it to Gray image and find faces
+while 1:
+    ret, img = cap.read()
+    cv2.resizeWindow('img', 500,400)
+    cv2.line(img,(500,200),(0,200),(0,255,0),1)
+    cv2.line(img,(250,0),(250,500),(0,255,0),1)
+    cv2.circle(img, (250, 200), 5, (255, 255, 255), -1)
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3)
+    cv2.rectangle(img,(230,180),(270,220),(0,255,0),5)
+#detect the face and make a rectangle around it.
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
+        roi_gray  = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        arr = {y:y+h, x:x+w}
+# Center of roi (Rectangle)
+        xx = int(x+(x+h))/2
+        yy = int(y+(y+w))/2
+        print ('X:')
+        print (xx)
+        print('Y:')
+        print (yy)
+        center = (xx,yy)
+# sending data to arduino
+        data = "X{0}Y{1}".format(xx, yy)
+        bdata = b'data'
+        print(data)
+        arr = bytes(data, 'ascii')
+        arduino.write(arr)
+        
+#Display the stream.
+    cv2.imshow('img',img)
+#Hit 'Esc' to terminate execution 
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+       break
---
+++ servo.ini/servo.ini.ino
@@ -0,0 +1,86 @@
+#include <Servo.h>
+#include <LiquidCrystal.h>
+Servo servoVer; //Vertical Servo
+Servo servoHor; //Horizontal Servo
+int servoYValue = 0;
+int x;
+int y;
+int prevX;
+int prevY;
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup()
+{ 
+  lcd.begin(16, 2); 
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  servoVer.attach(5); //Attach Vertical Servo to Pin 5
+  servoHor.attach(6); //Attach Horizontal Servo to Pin 6
+  servoVer.write(servoYValue);
+  servoHor.write(90); //This is currently confgiured for a continuous rotation servo.
+}
+void Pos()
+{ 
+  //Cube drawn in python represents a box of 40px
+  
+  if(x < 190){
+    //move right
+        servoHor.write(100);
+        delay(300); // This delay is how long the servo will move before stopping. The delay is higher because the coords of the face are further away and should move further.
+        servoHor.write(90);
+     }
+  if(x < 230 && x > 190){
+         servoHor.write(100);
+         delay(100);
+         servoHor.write(90);
+     } if(x > 270 && x < 290 ){ //move left
+      servoHor.write(85);
+      delay(100);
+      servoHor.write(90);
+
+    } if(x > 290){
+      //far away, faster wider movements.
+      servoHor.write(85);
+      delay(300);
+      servoHor.write(90);
+
+    }
+    if (y < 180){ //move up
+      if(servoYValue < 180){
+          servoYValue++;
+          servoVer.write(servoYValue);
+        }
+        
+    } if(y > 220) { //move down
+  if(servoYValue > 0){
+        servoYValue--;
+        servoVer.write(servoYValue);
+    }
+  }
+}
+
+
+
+
+void loop()
+{
+  if(Serial.available() > 0)
+  { 
+    if(Serial.read() == 'X')
+    {
+     lcd.print(x);
+      x = Serial.parseInt();
+      Serial.read();  //Lol moves forward two spaces in the data received from python, because its passed as a rounded decimal so we must skip the . and 0 (gets rounded, so is always .0)
+      Serial.read();      
+      if(Serial.read() == 'Y')
+      {
+       y = Serial.parseInt();
+       Pos();
+      }
+    }
+    while(Serial.available() > 0)
+    {
+      Serial.read();
+    }
+  }
+}
